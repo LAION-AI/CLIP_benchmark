9ed4870d39ae25f59bf8b372394f9e5a0b958182
Mehdi Cherti <m.cherti@fz-juelich.de>
2022-05-09 17:07:25 +0200

    - possibility to specify the split to use - verbose mode - more comments - add mean per class recal metric since it was used in a bunch of papers

--- clip_benchmark/cli.py
+++ clip_benchmark/cli.py
@@ -5,7 +5,7 @@ import json
 import torch
 import open_clip
 
-from clip_benchmark.datasets.builder import build_dataset, get_dataset_collate_fn, zeroshot_classification_templates
+from clip_benchmark.datasets.builder import build_dataset, get_dataset_collate_fn, get_zeroshot_classification_templates
 from clip_benchmark.metrics import zeroshot_classification, zeroshot_retrieval
 
 from torch.utils.data import default_collate
@@ -15,6 +15,7 @@ def main():
     """Console script for clip_benchmark."""
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str, default="cifar10", help="Dataset to use for the benchmark")
+    parser.add_argument('--split', type=str, default="test", help="Dataset split to use")
     parser.add_argument('--model', type=str, default="ViT-B-32-quickgelu", help="Model architecture to use from OpenCLIP")
     parser.add_argument('--pretrained', type=str, default="laion400m_e32", help="Model checkpoint name to use from OpenCLIP")
     parser.add_argument('--task', type=str, default="zeroshot_classification", choices=["zeroshot_classification", "zeroshot_retrieval"])
@@ -22,17 +23,30 @@ def main():
     parser.add_argument('--num_workers', default=4, type=int)
     parser.add_argument('--recall_k', default=[5], type=int, help="for retrieval, select the k for Recall@K metric. ", nargs="+",)
     parser.add_argument('--batch_size', default=64, type=int)
-    parser.add_argument('--dataset_root', default="root", type=str, help="dataset root")
-    parser.add_argument('--annotation_file', default="", type=str, help="text annotation file for retrieval datasets")
+    parser.add_argument('--dataset_root', default="root", type=str, help="dataset root folder where the datasets are downloaded.")
+    parser.add_argument('--annotation_file', default="", type=str, help="text annotation file for retrieval datasets. Only needed  for when `--task` is `zeroshot_retrieval`.")
     parser.add_argument('--output', default="result.json", type=str, help="output file where to dump the metrics")
+    parser.add_argument('--verbose', default=False, action="store_true", help="verbose mode")
 
     args = parser.parse_args()
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
     model, _, transform = open_clip.create_model_and_transforms(args.model, pretrained=args.pretrained)
     model = model.to(args.device)
-    dataset = build_dataset(args, transform=transform, train=False)
+    dataset = build_dataset(
+        dataset_name=args.dataset, 
+        root=args.dataset_root, 
+        transform=transform, 
+        split=args.split, 
+        annotation_file=args.annotation_file,
+        download=True,
+    )
     collate_fn = get_dataset_collate_fn(args.dataset)
-    print("Dataset size", len(dataset))
+    if args.verbose:
+        print(f"Dataset size: {len(dataset)}")
+        print(f"Dataset split: {args.split}")
+        print(f"Dataset classes: {dataset.classes}")
+        print(f"Dataset number of classes: {len(dataset.classes)}")
+
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=args.batch_size, 
         shuffle=False, num_workers=args.num_workers, 
@@ -40,7 +54,9 @@ def main():
     )
 
     if args.task == "zeroshot_classification":
-        zeroshot_templates = zeroshot_classification_templates.get(args.dataset)
+        zeroshot_templates = get_zeroshot_classification_templates(args.dataset)
+        if args.verbose:
+            print(f"Zero-shot templates: {zeroshot_templates}")
         classnames = dataset.classes if hasattr(dataset, "classes") else None
         assert (zeroshot_templates is not None and classnames is not None), "Dataset does not support classification"
         metrics = zeroshot_classification.evaluate(
@@ -48,7 +64,9 @@ def main():
             dataloader, 
             open_clip.tokenizer.tokenize, 
             classnames, zeroshot_templates, 
-            device=args.device, amp=args.amp
+            device=args.device, 
+            amp=args.amp,
+            verbose=args.verbose,
         )
     elif args.task == "zeroshot_retrieval":
         metrics = zeroshot_retrieval.evaluate(
@@ -56,10 +74,11 @@ def main():
             dataloader, 
             open_clip.tokenizer.tokenize, 
             recall_k_list=args.recall_k,
-            device=args.device, amp=args.amp
+            device=args.device, 
+            amp=args.amp
         )
     else:
-        raise ValueError("Unsupported task: {}".format(args.task))
+        raise ValueError("Unsupported task: {}. task should `zeroshot_classification` or `zeroshot_retrieval`".format(args.task))
     dump = {
         "dataset": args.dataset,
         "model": args.model,
--- clip_benchmark/metrics/zeroshot_classification.py
+++ clip_benchmark/metrics/zeroshot_classification.py
@@ -9,9 +9,33 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 
+from sklearn.metrics import classification_report, balanced_accuracy_score
 
 
 def zero_shot_classifier(model, tokenizer, classnames, templates, device):
+    """
+    This function returns zero-shot vectors for each class in order
+    to use it for zero-shot classification.
+    
+
+    model:
+        CLIP-like model with `encode_text`
+    
+    tokenizer:
+        text tokenizer
+    
+    classnames: list of str
+        name of classes
+    
+    templates: list of str
+        templates to use.
+    
+    Returns
+    -------
+    
+    torch.Tensor of shape (N,C) where N is the number
+    of templates, and C is the number of classes.
+    """
     with torch.no_grad():
         zeroshot_weights = []
         for classname in tqdm(classnames):
@@ -26,15 +50,52 @@ def zero_shot_classifier(model, tokenizer, classnames, templates, device):
 
 
 def accuracy(output, target, topk=(1,)):
+    """
+    Compute top-k accuracy
+
+    output: torch.Tensor
+        shape (N, C) where N is the number of examples, C the number of classes.
+        these are the logits.
+    
+    target: torch.Tensor
+        shape (N,) where N is the number of examples. Class id of each example.
+    
+    topk: tuple
+        which topk to compute, topk
+    Returns
+    -------
+    
+    list of top-k accuracies in the same order of `topk`
+    """
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
-    return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
+    return [float(correct[:k].reshape(-1).float().mean(0, keepdim=True).cpu().numpy()) for k in topk]
 
 
 def run_classification(model, classifier, dataloader, device, amp=False):
+    """
+    Run zero-shot classifcation
+
+    model: torch.nn.Module
+        CLIP-like model with `encode_image` and `encode_text`
+    
+    classifier: torch.Tensor
+        obtained from `zero_shot_classifier`
+    
+    dataloader: torch.utils.data.Dataloader 
+        torch
+    
+    Returns
+    -------
+    (pred, true)  where
+        - pred (N, C) are the logits
+        - true (N,) are the actual classes
+    """
     autocast = torch.cuda.amp.autocast if amp else suppress
+    pred = []
+    true = []
+    nb = 0
     with torch.no_grad():
-        top1, top5, n = 0., 0., 0.
         for images, target in tqdm(dataloader):
             images = images.to(device)
             target = target.to(device)
@@ -44,21 +105,53 @@ def run_classification(model, classifier, dataloader, device, amp=False):
                 image_features = model.encode_image(images)
                 image_features = F.normalize(image_features, dim=-1)
                 logits = 100. * image_features @ classifier
+            
+            true.append(target.cpu())
+            pred.append(logits.cpu())
 
-            # measure accuracy
-            if len(dataloader.dataset.classes) >= 5:
-                acc1, acc5 = accuracy(logits, target, topk=(1, 5))
-            else:
-                acc1, = accuracy(logits, target, topk=(1,))
-                acc5 = float("nan")
-            top1 += acc1
-            top5 += acc5
-            n += images.size(0)
+    pred = torch.cat(pred)
+    true = torch.cat(true)
+    return pred, true
 
-    top1 = (top1 / n)
-    top5 = (top5 / n)
-    return {'top1_zeroshot_accuracy': top1, 'top5_zeroshot_accuracy': top5}
+def evaluate(model, dataloader, tokenizer, classnames, templates, device, amp=False, verbose=False):
+    """
+    Run zero-shot classification and evalue the metrics
 
-def evaluate(model, dataloader, tokenizer, classnames, templates, device, amp=False):
+    model: torch.nn.Module
+        CLIP-like model with `encode_image` and `encode_text`
+    
+    dataloader: torch.utils.data.Dataloader
+
+    tokenizer: text tokenizer
+
+    classnames: list of str
+        class names
+    
+    templates: list of str
+        templates to use for zero-shot classification
+    
+    device: cpu/cuda
+
+    amp: whether to use automatic mixed precision
+
+    verbose: whether to use verbose model
+
+    Returns
+    -------
+
+    dict of classification metrics
+    """
     classifier = zero_shot_classifier(model, tokenizer, classnames, templates, device)
-    return run_classification(model, classifier, dataloader, device, amp=amp)
+    logits, target = run_classification(model, classifier, dataloader, device, amp=amp)
+    pred = logits.argmax(axis=1)
+    # measure accuracy
+    if len(dataloader.dataset.classes) >= 5:
+        acc1, acc5 = accuracy(logits, target, topk=(1, 5))
+    else:
+        acc1, = accuracy(logits, target, topk=(1,))
+        acc5 = float("nan") 
+    mean_per_class_recall = balanced_accuracy_score(target, pred)
+    if verbose:
+        print(classification_report(target, pred, digits=3))
+    return {"acc1": acc1, "acc5": acc5, "mean_per_class_recall": mean_per_class_recall}
+
