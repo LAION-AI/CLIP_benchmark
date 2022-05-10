37311fd380c204b8cc7cce8c0370a9f9c2d7bab6
Mehdi Cherti <m.cherti@fz-juelich.de>
2022-05-10 11:42:17 +0200

    correct top5 as it was before

--- clip_benchmark/metrics/zeroshot_classification.py
+++ clip_benchmark/metrics/zeroshot_classification.py
@@ -69,7 +69,8 @@ def accuracy(output, target, topk=(1,)):
     """
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
-    return [float(correct[:k].reshape(-1).float().mean(0, keepdim=True).cpu().numpy()) for k in topk]
+    n = len(target)
+    return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) / n for k in topk]
 
 
 def run_classification(model, classifier, dataloader, device, amp=False):
