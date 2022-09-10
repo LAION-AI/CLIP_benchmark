dddafb97ef866f623d4df674c3db6a52051c76c0
Romain Beaumont <romain.rom1@gmail.com>
2022-09-10 19:40:53 +0200

    support zero shot retrieval in run.sh

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -9,10 +9,18 @@ ROOT=clip_benchmark_datasets
 for ds in $DATASETS;do
     ds_name=`echo $ds|tr '/' '_'`
     for model in $MODELS;do
-        arch=$(echo $model|cut -d, -f1)
-        pretrained=$(echo $model|cut -d, -f2)
-        echo $ds $pretrained $arch
-        $RUN --dataset=$ds --dataset_root $ROOT/$ds --task=zeroshot_classification --pretrained=$pretrained --model=$arch --output="${ds_name}_${pretrained}_${model}.json"  --batch_size=$BS --num_workers=$WORKERS
+       arch=$(echo $model|cut -d, -f1)
+       pretrained=$(echo $model|cut -d, -f2)
+       echo $ds $pretrained $arch
+       if [[ "$ds_name" == "flickr30k" || "$ds_name" == "flickr8k" || "$ds_name" == "mscoco_captions" ]]
+       then
+               TASK=zeroshot_retrieval
+       else
+               TASK=zeroshot_classification
+       fi
+       echo "$ds_name"
+       echo $TASK
+       $RUN --dataset=$ds --annotation_file=$ROOT/$ds/captions.txt --dataset_root $ROOT/$ds --task=$TASK --pretrained=$pretrained --model=$arch --output="${ds_name}_${name}_${arch}.json"  --batch_size=$BS --num_workers=$WORKERS
     done
 done
 python build_csv.py
