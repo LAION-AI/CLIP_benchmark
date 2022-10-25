a61e674656e84a94a93e9c0fa5990bb2140cee75
Mitchell Noss Wortsman <mitchellw@devfair0174.h2.fair>
2022-10-25 16:11:37 -0700

    minor plot changes

--- probe_benchmark/scaling_plot.py
+++ probe_benchmark/scaling_plot.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from tokenize import group
 import numpy as np
 import pandas as pd
@@ -6,6 +7,8 @@ import matplotlib.pyplot as plt
 
 from matplotlib.gridspec import GridSpec
 import matplotlib.pyplot as plt
+
+from matplotlib.lines import Line2D
 
 import statsmodels.api as sm
 
@@ -27,8 +30,14 @@ if __name__ == '__main__':
     fig = plt.figure(constrained_layout=True, figsize=(5*3, 3.5*2))
     gs = GridSpec(2, 3, figure=fig)
 
+
+
     for i1 in range(2):
         for i2 in range(3):
+
+            clip_legend = False
+            laion2b_legend = False
+            laion400m_legend = False
             ax = fig.add_subplot(gs[i1, i2])
 
             pdf = df[df.dataset == datasets[i1]]
@@ -45,16 +54,41 @@ if __name__ == '__main__':
                     txt.append(subgroupdf['model_short'].values[0])
 
                 xs, ys = np.array(xs), np.array(ys)
-                ax.scatter(xs, ys, label=name.replace('_e32', ''), color=f'C{j}')
                 for i in range(len(xs)):
-                    ax.annotate(txt[i], (xs[i], ys[i] + 0.0035), color=f'C{j}')
+                    print(txt[i])
+                    color = 'C0' if 'CLIP' in txt[i] else 'C1'
+                    marker = 's' if '2B' in txt[i] else 'o'
+                    if 'B/32' in txt[i]:
+                        sz = 1
+                    elif 'B/16' in txt[i]:
+                        sz = 2
+                    elif 'B/16+' in txt[i]:
+                        sz = 3
+                    elif 'L/14' in txt[i]:
+                        sz = 4
+                    elif 'H/14'in txt[i]:
+                        sz = 5
 
+                    label = None
+                    if '2B' in txt[i] and not laion2b_legend:
+                        label = 'LAION 2B'
+                        laion2b_legend = True
+                    elif '400M' in txt[i] and not laion400m_legend:
+                        label = 'LAION 400M'
+                        laion400m_legend = True
+                    elif 'CLIP' in txt[i] and not clip_legend:
+                        label = 'CLIP WiT 400M'
+                        clip_legend = True
+                    
+                    
+                    ax.scatter(xs[i], ys[i], marker=marker, color=color, s = 30 * sz, label=label)
+                        
                 lin_params, _ = linear_fit(np.log(xs), ys)
                 xs = np.log(np.array([xs.min(), xs.max()]))
                 ys = lin_params[1] * xs + lin_params[0]
                 ax.plot(np.exp(xs), ys, color=f'C{j}')
 
-            ax.legend()
+
             ax.set_xscale('log')
             ax.set_xlabel('Total compute (GMACS per sample x samples seen)')
             ax.set_ylabel('Accuracy')
@@ -65,6 +99,7 @@ if __name__ == '__main__':
                 ax.set_title(f'dataset = {dset}, {ks[i2]}-shot.')
             ax.grid()
 
+    ax.legend()
     plt.savefig(
         f"probe_benchmark/scaling_plot.png", bbox_inches="tight"
     )
