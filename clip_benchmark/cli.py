3999bc489d4c55d9486a37069c0b60887baaa65c
Fredrik Carlsson <FreddeFc@gmail.com>
2022-11-15 19:25:15 +0100

    Removed line breaks?

--- clip_benchmark/cli.py
+++ clip_benchmark/cli.py
@@ -33,8 +33,8 @@ def main():
     parser.add_argument('--annotation_file', default="", type=str, help="text annotation file for retrieval datasets. Only needed  for when `--task` is `zeroshot_retrieval`.")
     parser.add_argument('--language', default="en", type=str, help="language of classname and prompts to use for zeroshot classification.")
     parser.add_argument('--output', default="result.json", type=str, help="output file where to dump the metrics")
-    parser.add_argument('--verbose', default=False, action="store_true", help="verbose mode")
     parser.add_argument('--model_cache_dir', type=str, help="directory to where downloaded models are cached")
+    parser.add_argument('--verbose', default=False, action="store_true", help="verbose mode")
     args = parser.parse_args()
     run(args)
 
@@ -42,17 +42,17 @@ def main():
 def run(args):
     """Console script for clip_benchmark."""
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
-
     # set seed.
     torch.manual_seed(args.seed)
     if args.skip_load:
         model, transform, collate_fn, dataloader = None, None, None, None
     else:
-        model, _, transform = open_clip.create_model_and_transforms(args.model, pretrained=args.pretrained, cache_dir=args.model_cache_dir)
+        model, _, transform = open_clip.create_model_and_transforms(args.model, pretrained=args.pretrained)
         model = model.to(args.device)
         tokenizer = open_clip.get_tokenizer(args.model)
         dataset = build_dataset(
-            dataset_name=args.dataset, root=args.dataset_root,
+            dataset_name=args.dataset,
+            root=args.dataset_root,
             transform=transform,
             split=args.split,
             annotation_file=args.annotation_file,
@@ -77,76 +77,74 @@ def run(args):
             collate_fn=collate_fn
         )
 
-        if args.task == "zeroshot_classification":
-            zeroshot_templates = dataset.templates if hasattr(dataset, "templates") else None
-            if args.verbose:
-                print(f"Zero-shot templates: {zeroshot_templates}")
-            classnames = dataset.classes if hasattr(dataset, "classes") else None
-            assert (
-                    zeroshot_templates is not None and classnames is not None), "Dataset does not support classification"
-            metrics = zeroshot_classification.evaluate(
-                model,
-                dataloader,
-                tokenizer,
-                classnames, zeroshot_templates,
-                device=args.device,
-                amp=args.amp,
-                verbose=args.verbose,
-            )
-        elif args.task == "zeroshot_retrieval":
-            metrics = zeroshot_retrieval.evaluate(
-                model,
-                dataloader,
-                tokenizer,
-                recall_k_list=args.recall_k,
-                device=args.device,
-                amp=args.amp
-            )
-        elif args.task == "linear_probe":
-            # we also need the train split for linear probing.
-            train_dataset = build_dataset(
-                dataset_name=args.dataset,
-                root=args.dataset_root,
-                transform=transform,
-                split='train',
-                annotation_file=args.annotation_file,
-                download=True,
-            )
-            train_dataloader = torch.utils.data.DataLoader(
-                train_dataset, batch_size=args.batch_size,
-                shuffle=False, num_workers=args.num_workers,
-                collate_fn=collate_fn, pin_memory=True,
-            )
-            metrics = linear_probe.evaluate(
-                model,
-                train_dataloader,
-                dataloader,
-                args.fewshot_k,
-                args.batch_size,
-                args.num_workers,
-                args.fewshot_lr,
-                args.fewshot_epochs,
-                (args.model + '-' + args.pretrained + '-' + args.dataset).replace('/', '_'),
-                args.seed,
-                args.feature_root,
-                device=args.device,
-                amp=args.amp,
-                verbose=args.verbose,
-            )
-        else:
-            raise ValueError(
-                "Unsupported task: {}. task should `zeroshot_classification` or `zeroshot_retrieval`".format(args.task))
-        dump = {
-            "dataset": args.dataset,
-            "model": args.model,
-            "pretrained": args.pretrained,
-            "task": args.task,
-            "metrics": metrics,
-            "language": args.language,
-        }
-        with open(args.output, "w") as f:
-            json.dump(dump, f)
-        return 0
+    if args.task == "zeroshot_classification":
+        zeroshot_templates = dataset.templates if hasattr(dataset, "templates") else None
+        if args.verbose:
+            print(f"Zero-shot templates: {zeroshot_templates}")
+        classnames = dataset.classes if hasattr(dataset, "classes") else None
+        assert (zeroshot_templates is not None and classnames is not None), "Dataset does not support classification"
+        metrics = zeroshot_classification.evaluate(
+            model,
+            dataloader,
+            tokenizer,
+            classnames, zeroshot_templates,
+            device=args.device,
+            amp=args.amp,
+            verbose=args.verbose,
+        )
+    elif args.task == "zeroshot_retrieval":
+        metrics = zeroshot_retrieval.evaluate(
+            model,
+            dataloader,
+            tokenizer,
+            recall_k_list=args.recall_k,
+            device=args.device,
+            amp=args.amp
+        )
+    elif args.task == "linear_probe":
+        # we also need the train split for linear probing.
+        train_dataset = build_dataset(
+            dataset_name=args.dataset,
+            root=args.dataset_root,
+            transform=transform,
+            split='train',
+            annotation_file=args.annotation_file,
+            download=True,
+        )
+        train_dataloader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=args.batch_size,
+            shuffle=False, num_workers=args.num_workers,
+            collate_fn=collate_fn, pin_memory=True,
+        )
+        metrics = linear_probe.evaluate(
+            model,
+            train_dataloader,
+            dataloader,
+            args.fewshot_k,
+            args.batch_size,
+            args.num_workers,
+            args.fewshot_lr,
+            args.fewshot_epochs,
+            (args.model + '-' + args.pretrained + '-' + args.dataset).replace('/', '_'),
+            args.seed,
+            args.feature_root,
+            device=args.device,
+            amp=args.amp,
+            verbose=args.verbose,
+        )
+    else:
+        raise ValueError("Unsupported task: {}. task should `zeroshot_classification` or `zeroshot_retrieval`".format(args.task))
+    dump = {
+        "dataset": args.dataset,
+        "model": args.model,
+        "pretrained": args.pretrained,
+        "task": args.task,
+        "metrics": metrics,
+        "language": args.language,
+    }
+    with open(args.output, "w") as f:
+        json.dump(dump, f)
+    return 0
 
 
 if __name__ == "__main__":
