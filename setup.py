a6947931d038fe6d596890e7e99f1197f269a8db
Romain Beaumont <romain.rom1@gmail.com>
2022-11-17 22:18:35 +0100

    pypi description as markdown

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='clip_benchmark',
     name='clip_benchmark',
