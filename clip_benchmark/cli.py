ee47023150aa6deec11d680b3624095996d10657
Romain Beaumont <romain.rom1@gmail.com>
2022-12-26 22:53:55 +0100

    use model_collection in cli.py

--- clip_benchmark/cli.py
+++ clip_benchmark/cli.py
@@ -5,7 +5,8 @@ import json
 import torch
 from clip_benchmark.datasets.builder import build_dataset, get_dataset_collate_fn, get_dataset_default_task, dataset_collection, get_dataset_collection_from_file
 from clip_benchmark.metrics import zeroshot_classification, zeroshot_retrieval, linear_probe
-from clip_benchmark.models import model_collection, get_model_collection_from_file, model_collection, load_clip, MODEL_TYPES
+from clip_benchmark.model_collection import model_collection, get_model_collection_from_file, model_collection
+from clip_benchmark.models import load_clip, MODEL_TYPES
 
 def get_parser_args():
     parser = argparse.ArgumentParser()
