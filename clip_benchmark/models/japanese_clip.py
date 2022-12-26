e97e67829ab762de7dd9a7a0212448f9c12729cd
mkshing <mktshing4@gmail.com>
2022-12-26 11:38:30 +0900

    add wrapper for ja_clip

--- clip_benchmark/models/japanese_clip.py
+++ clip_benchmark/models/japanese_clip.py
@@ -2,6 +2,31 @@ from typing import Dict
 import torch
 
 
+class DictTensor:
+    """
+    enable to do `tokenizer(texts).to(device)`
+    """
+    def __init__(self, d: Dict[str, torch.Tensor]):
+        self.d = d
+
+    def to(self, device):
+        return {k: v.to(device) for k, v in self.d.items()}
+
+
+class JapaneseCLIPForBenchmark:
+    """
+    enable to do model.encode_text(dict_tensor)
+    """
+    def __init__(self, model):
+        self.model = model
+
+    def encode_text(self, dict_tensor):
+        return self.model.get_text_features(**dict_tensor)
+
+    def encode_image(self, image):
+        return self.model.get_image_features(image)
+
+
 def load_japanese_clip(pretrained: str, device="cpu", **kwargs):
     """
     Load Japanese CLIP/CLOOB by rinna (https://github.com/rinnakk/japanese-clip)
@@ -12,7 +37,8 @@ def load_japanese_clip(pretrained: str, device="cpu", **kwargs):
         import japanese_clip as ja_clip
     except ImportError:
         raise ImportError("Install `japanese_clip` by `pip install git+https://github.com/rinnakk/japanese-clip.git`")
-    model, transform = ja_clip.load(pretrained, device=device, **kwargs)
+    cache_dir = kwargs.pop("cache_dir", None)
+    model, transform = ja_clip.load(pretrained, device=device, cache_dir=cache_dir)
 
     class JaTokenizerWrapper:
         def __init__(self, ):
@@ -20,9 +46,9 @@ def load_japanese_clip(pretrained: str, device="cpu", **kwargs):
 
         def __call__(self, texts) -> Dict[str, torch.Tensor]:
             inputs = ja_clip.tokenize(texts, tokenizer=self.tokenizer, device="cpu")
-            return inputs
+            return DictTensor(inputs)
 
         def __len__(self):
             return len(self.tokenizer)
 
-    return model, transform, JaTokenizerWrapper()
+    return JapaneseCLIPForBenchmark(model), transform, JaTokenizerWrapper()
