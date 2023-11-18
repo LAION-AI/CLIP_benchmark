55a7d824fedaebc6d20b44111f6237cba01c9d51
Alexander Visheratin <alexvish91@gmail.com>
2023-11-18 18:36:02 -0500

    Fixed xm3600.

--- clip_benchmark/datasets/crossmodal3600.py
+++ clip_benchmark/datasets/crossmodal3600.py
@@ -67,7 +67,7 @@ class Crossmodal3600(VisionDataset):
         img, captions = self.data[index]
 
         # Image
-        img = Image.open(os.path.join(self.root, img)).convert("RGB")
+        img = Image.open(img).convert("RGB")
         if self.transform is not None:
             img = self.transform(img)
 
