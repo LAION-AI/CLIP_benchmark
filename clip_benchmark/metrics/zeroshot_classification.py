ee14a9a8a9bcde09d51f4a4b1dff5a75182a3596
Mehdi Cherti <mehdicherti@gmail.com>
2026-07-23 16:21:07 +0200

    fix TypeError: only 0-dimensional arrays can be converted to Python scalars issue in zero-shot clf

--- clip_benchmark/metrics/zeroshot_classification.py
+++ clip_benchmark/metrics/zeroshot_classification.py
@@ -79,7 +79,7 @@ def accuracy(output, target, topk=(1,)):
     pred = output.topk(max(topk), 1, True, True)[1].t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
     n = len(target)
-    return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) / n for k in topk]
+    return [float(correct[:k].reshape(-1).float().sum().cpu().numpy()) / n for k in topk]
 
 
 def run_classification(model, classifier, dataloader, device, amp=True):
